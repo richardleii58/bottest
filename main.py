1df73b77f20c3121d06cfb9ad45e3eaf7e0fcc0b
Lei Sihan Richard <richardlei.2023@scis.smu.edu.sg>
2024-07-05 22:26:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import datetime
 import os
 import psycopg2
 from urllib.parse import urlparse
-## import keep_alive
+##import keep_alive
 
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ['TELEGRAM_TOKEN']
 URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 DATABASE_URL = os.environ['DATABASE_URL']
@@ -186,7 +186,15 @@ def echo_all(updates):
         except Exception as e:
             print(e)
 
-
+def main():
+    ##keep_alive.keep_alive()
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if "result" in updates and len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
